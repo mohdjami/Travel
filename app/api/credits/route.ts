dbe909f962cf1861646061a52b3a76f77963bb4b
mohd jami <your_email@example.com>
2024-09-24 20:47:59 +0530

    Enhancements and Refactoring

--- app/api/credits/route.ts
+++ app/api/credits/route.ts
@@ -3,20 +3,25 @@ import { getServerUser } from "@/utils/users/server";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, res: Response) {
-  const user = await getServerUser();
-  const supabase = createClient();
-  const { data, error } = await supabase
-    .from("users")
-    .select("credits")
-    .eq("id", user?.id)
-    .single();
-  if (!data) {
-    console.log("User not found");
-    return NextResponse.json({ error: "User not found" });
-  }
-  if (error) {
+  try {
+    const user = await getServerUser();
+    const supabase = createClient();
+    const { data, error } = await supabase
+      .from("users")
+      .select("credits")
+      .eq("id", user?.id)
+      .single();
+    if (!data) {
+      console.log("User not found");
+      return NextResponse.json({ error: "User not found" });
+    }
+    if (error) {
+      console.log(error);
+      return NextResponse.json({ error });
+    }
+    return NextResponse.json({ credits: data?.credits });
+  } catch (error) {
     console.log(error);
     return NextResponse.json({ error });
   }
-  return NextResponse.json({ credits: data?.credits });
 }
